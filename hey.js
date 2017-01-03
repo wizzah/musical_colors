5b5a8a7b1ddae0caf1b4606da1252434debe38ff
Elizabeth Williams <elizabeth.williams@ucf.edu>
2017-01-02 22:09:07 -0500

    Added opacity, using polyphonic pressure

--- hey.js
+++ hey.js
@@ -5,6 +5,7 @@
 
 var width = 100;
 var height = 100;
+var main_viz;
 
 // generate random points
 function generate_points(n)
@@ -45,7 +46,7 @@ var octave_dict = {
     7: ["#990099"],
     8: ["#cc0066"]
 };
-function draw_svg(note_name, note_octave) {
+function draw_svg(note_name, note_octave, rawVelocity) {
     rand_points = generate_points(1);
 
     // create svg drawing
@@ -54,20 +55,35 @@ function draw_svg(note_name, note_octave) {
     var old = document.getElementsByClassName('art')[0];
     document.body.replaceChild(svgEl, old);
     var draw = SVG(svgEl);
-    var rect = draw.rect(rand_points[0].x, rand_points[0].y);
-
-    console.log(note_name, note_octave, notes_dict[note_name][0], octave_dict[note_octave][0]);
+    main_viz = draw;
+    main_viz.opacity((100-rawVelocity)/100);
+    console.log((100-rawVelocity)/100);
+    // var rect = draw.rect(rand_points[0].x, rand_points[0].y);
+    var rect = draw.rect(rawVelocity, rand_points[0].y);
+    // console.log(note_name, note_octave, notes_dict[note_name][0], octave_dict[note_octave][0]);
     var gradient = draw.gradient('linear', function(stop) {
         stop.at(0, notes_dict[note_name][0])
         stop.at(1, octave_dict[note_octave][0])
     });
-    console.log(gradient);
+    // console.log(gradient);
     rect.attr({ fill: gradient });
 
     draw.viewbox({ x:0, y:0, width:100, height:100 });
 
     var group = draw.group();
 }
+
+function edit_opac(opac) {
+    main_viz.opacity(opac);
+}
+// noteon: play a note;
+// noteoff: stop playing a note;
+// keyaftertouch: alter the pressure value associated with a specific note;
+// controlchange: send control information (modulation, volume, pan, etc.);
+// channelmode: send channel status (all sounds off, reset, all notes off, etc.);
+// programchange: change to a specific patch/program number;
+// channelaftertouch: alter the global pressure associated with a channel;
+// pitchbend: bend the pitch of the sound by a certain amount.
 
 WebMidi.enable(function (err) {
 
@@ -81,10 +97,22 @@ WebMidi.enable(function (err) {
     console.log(input);
      input.addListener('noteon', "all",
         function (e) {
-          console.log("Received 'noteon' message (" + e.note.name + e.note.octave + ").");
-          draw_svg(e.note.name, e.note.octave);
+            console.log(e);
+            draw_svg(e.note.name, e.note.octave, e.rawVelocity);
+          // console.log(e);
+          // console.log(e.type);
         }
       );
+
+     input.addListener('channelaftertouch', 'all',
+        function(e) {
+            //modify opacity just to see effect
+            // console.log(SVG.get(main_viz));
+            edit_opac((100-e.data[1])/100);
+            // draw_svg(e.note.name, e.note.octave, e.rawVelocity);
+        }
+    );
+
   }
 
 });
